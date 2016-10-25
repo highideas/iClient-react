816cfc4e63bc0d77bf47f89a0e5d522b000f919c
Marco Araujo <marco.araujo.junior@gmail.com>
2016-10-25 11:20:45 -0200

    Toggle when mobile view

--- app/components/Menu/Menu.js
+++ app/components/Menu/Menu.js
@@ -6,11 +6,28 @@ class Menu extends React.Component{
         super(props, context);
         this.handleLogout = this.handleLogout.bind(this);
         this.handleView = this.handleView.bind(this);
+        this.toggleNav = this.toggleNav.bind(this);
+        this.handleLink = this.handleLink.bind(this);
+        this.state = {
+            toggleNav: ''
+        };
     }
 
     handleLogout() {
         delete localStorage.token;
         this.context.router.push("/");
+    }
+
+    toggleNav() {
+        if (this.state.toggleNav) {
+            this.setState({toggleNav: ''});
+        } else {
+            this.setState({toggleNav: 'is-active'});
+        }
+    }
+
+    handleLink(e) {
+        this.setState({toggleNav: ''});
     }
 
     handleView() {
@@ -20,15 +37,28 @@ class Menu extends React.Component{
                     <div className="nav-left">
                         <a className="nav-item" href="../index.html">IClient</a>
                     </div>
-                    <span className="nav-toggle">
+                    <span className={ `nav-toggle ${this.state.toggleNav}` } onClick={this.toggleNav}>
                         <span></span>
                         <span></span>
                         <span></span>
                     </span>
-                    <div className="nav-right nav-menu">
-                        <Link to="/" className="nav-item is-tab is-active">Home</Link>
-                        <Link to="/client" className="nav-item is-tab">Client</Link>
-                        <Link to="/area" className="nav-item is-tab">Area</Link>
+                    <div className={ `nav-right nav-menu ${this.state.toggleNav}` }>
+                        <Link to="/" 
+                            className="nav-item is-tab"
+                            onlyActiveOnIndex={true}
+                            activeClassName="is-active"
+                            onClick={this.handleLink}>Home
+                        </Link>
+                        <Link to="/client"
+                            className="nav-item is-tab"
+                            activeClassName="is-active"
+                            onClick={this.handleLink}>Client
+                        </Link>
+                        <Link to="/area"
+                            className="nav-item is-tab"
+                            activeClassName="is-active"
+                            onClick={this.handleLink}>Area
+                        </Link>
                         <span className="nav-item">
                             <a className="button" onClick={this.handleLogout}>Logout</a>
                         </span>
