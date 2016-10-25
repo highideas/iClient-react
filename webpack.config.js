66164bdcb86a7662a4fd82b1e4d9b1e0269bbd8a
Marco Araujo <marco.araujo.junior@gmail.com>
2016-10-25 09:48:33 -0200

    Add font-awesome into app

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,18 @@ const path = require('path');
 module.exports = {
     entry: "./app/App.js",
     output: {
-        filename: "public/dist/bundle.js"
+        path: 'public/dist/',
+        publicPath: '/dist/',
+        filename: "bundle.js",
     },
     module: {
         loaders: [
+            //Bulma loader
             { test: /\.css$/, loader: "style-loader!css-loader" },
+            //Font-awesome loader
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+            //Jsx loader
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
