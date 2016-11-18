f8467d2fa5eb6de5ec3e776a6d0728f612db7d0f
Marco Araujo <marco.araujo.junior@gmail.com>
2016-11-18 09:55:07 -0200

    Using card instead table to show clients

--- app/components/Client/Client.js
+++ app/components/Client/Client.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CardStack, Card } from 'react-cardstack';
 
 import ClientService from 'services/Client';
 import Error from 'components/Error/Error'
@@ -8,7 +9,7 @@ class Client extends React.Component
     constructor(props) {
         super(props);
         this.state = {
-            clients : [],
+            clients : null,
             error : ''
         };
         this.getClients();
@@ -29,34 +30,36 @@ class Client extends React.Component
         if (this.state.error) {
             return (<Error error={this.state.error} />);
         }
+        if (!this.state.clients) {
+            return <div>Loading...</div>;
+        }
         const clientList = this.state.clients.map((client, key) => {
             return (
-                <tr key={key} >
-                    <td>{ client.name }</td>
-                    <td>{ client.address }</td>
-                    <td>{ client.city }</td>
-                    <td className="is-icon"> <a href="#"> <i className="fa fa-calendar"></i> </a> </td>
-                    <td className="is-icon"> <a href="#"> <i className="fa fa-search"></i>   </a> </td>
-                </tr>
+                <Card key={key}>
+                    <article className={`message ${((key % 2) ? 'is-success' : 'is-info')}`}>
+                        <div className="message-header">
+                            <strong>{ client.name }</strong>
+                        </div>
+                        <div className="message-body">
+                            <address>{ client.address }</address>
+                            <city><small>{ client.city }</small></city>
+                        </div>
+                    </article>
+                </Card>
             );
         });
 
         return (
             <div className="container hello">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>City</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { clientList }
-                    </tbody>
-                </table>
+                <CardStack
+                    height={100}
+                    width={1200}
+                    background='#00d1b2'
+                    hoverOffset={0}>
+
+                    { clientList }
+
+                </CardStack>
             </div>
         );
     }
