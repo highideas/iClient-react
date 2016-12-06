a9c6f505d4aaceaa63e11fe71bd06e7aef66baff
Marco Araujo <marco.araujo.junior@gmail.com>
2016-12-06 21:04:48 -0200

    Prepare to migrate to webpack from 1 to 2 version

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,48 @@ const path = require('path');
 
 var webpack = require('webpack');
 
+var bulmaLoader = {
+    test: /\.css$/,
+    loader: "style-loader!css-loader"
+};
+
+var fontAwesomeLoader = {
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: "file-loader"
+};
+
+var fontAwesomeWoffLoader = {
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: "url-loader?limit=10000&minetype=application/font-woff"
+};
+
+var jsxLoader = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel',
+    query: {
+        presets: ['react', 'es2015']
+    }
+};
+
+var uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+});
+
+var definePlugin = new webpack.DefinePlugin({
+    HOST: JSON.stringify(process.env.HOST || 'http://localhost:3000'),
+    'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+    }
+});
+
+var resolve = {
+    extensions: ['', '.js', '.jsx'],
+    root: [
+        path.resolve('./app')
+    ]
+};
+
 module.exports = {
     entry: "./app/App.js",
     output: {
@@ -11,37 +53,15 @@ module.exports = {
     },
     module: {
         loaders: [
-            //Bulma loader
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            //Font-awesome loader
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
-            //Jsx loader
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            }
+            bulmaLoader,
+            fontAwesomeWoffLoader,
+            fontAwesomeLoader,
+            jsxLoader
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        }),
-        new webpack.DefinePlugin({
-            HOST: JSON.stringify(process.env.HOST || 'http://localhost:3000'),
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
+        uglifyJsPlugin,
+        definePlugin
     ],
-    resolve: {
-        extensions: ['', '.js', '.jsx'],
-        root: [
-            path.resolve('./app')
-        ]
-    }
+    resolve: resolve
 }
